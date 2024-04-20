dd1ec8e54898fbe817e7bfb5929fd88ba8c88091
Philip Fibranz <philip.fibranz@nexteer.com>
2024-04-20 17:38:44 +0000

    start of simple sorting alg

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    println!("Sort these");
+    
+    let numarr = [32, 24, 45, 2, 8, 33, 12, 4, 298, 125, 186, 250];
+    println!("{:?}", numarr);
+    let output: [i32; 12];
+
+    for i in 0..numarr.len() {
+
+    }
+    
+    println!("{:?}", output);
+}
